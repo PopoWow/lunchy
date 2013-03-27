92dba0d83f50d1d9c3696d3b0431e2f45ab8e928
Kyle Kawamura <kylekawa@gmail.com>
2013-03-27 00:55:09 -0700

    Initial commit to get this project started

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lunchy::Application.config.secret_token = 'cdbd12949ef238cc9b1484901d9db55fc43efa2bc66d7a3bc29d28295f6fb4c2207827781a7e92d79cac64d157c53bfd55eeb64f91ecfd398bf81baf2a0defec'
