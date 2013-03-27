95f459f5059448058ff2699beeab0a67c6a1b33c
popowow <comrademisc@gmail.com>
2013-03-27 10:04:52 -0700

    Converted to HAML.  Added restaurant scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Lunchy::Application.routes.draw do
+  resources :restaurants
+
+
+  get "home/index"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,7 +53,7 @@ Lunchy::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'home#index'
 
   # See how all your routes lay out with "rake routes"
 
