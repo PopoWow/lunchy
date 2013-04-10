55eeb7079710cb56fdabf62a033fc1bb52ab2467
popowow <comrademisc@gmail.com>
2013-04-10 13:10:41 -0700

    Add :through associations for :restaurant/:course.

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,4 +1,5 @@
 class Course < ActiveRecord::Base
   has_many :dishes
+  has_one :restaurant, through => :dishes
   attr_accessible :description, :name, :waiter_id
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,4 +1,5 @@
 class Restaurant < ActiveRecord::Base
   has_many :dishes
+  has_many :courses, :through => :dishes
   attr_accessible :address, :description, :food_type, :logo_image, :logo_url, :name, :waiter_id
 end
