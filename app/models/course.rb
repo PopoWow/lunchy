d3e1c4ea8dff64f618ea6c84f2a0b2a1c8ea88d6
popowow <comrademisc@gmail.com>
2013-04-11 18:34:50 -0700

    Add dependent/destroy to models

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,5 +1,5 @@
 class Course < ActiveRecord::Base
-  has_many :dishes
+  has_many :dishes, :dependent => :destroy
   belongs_to :restaurant
   attr_accessible :description, :name, :waiter_id
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,5 +1,5 @@
 class Restaurant < ActiveRecord::Base
-  has_many :courses
+  has_many :courses, :dependent => :destroy
   has_many :dishes, :through => :courses
   attr_accessible :address, :description, :food_type, :logo_image, :logo_url, :name, :waiter_id
 end
