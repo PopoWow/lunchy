cadfc7fe5da198661ee0e35534a55dbae520b902
Kyle Kawamura <kylekawa@gmail.com>
2013-05-13 15:20:15 -0700

    add get_yelp_business_info

--- lib/yelp/yelp_access.rb
+++ lib/yelp/yelp_access.rb
@@ -14,6 +14,7 @@ module YelpAccess
                                   yelp_info[:token_secret])
   end
 
+  # Using this to automatically initialize class variable
   @@access_token = self.init_access_token
 
   def get_yelp_id(name, address)
@@ -28,6 +29,13 @@ module YelpAccess
         puts "Found more than one match (#{data["businesses"].length}) for #{name}"
       end
       return data["businesses"][0]["id"]
+    end
+  end
+
+  def get_yelp_business_info(yelp_id)
+    if yelp_id and not yelp_id.empty?
+      biz_uri = BUSINESS_PAT % yelp_id
+      data = JSON.parse(@@access_token.get(biz_uri).body)
     end
   end
 end
