6b5715812e6f44bfbb21027f9a221d580b307421
Kyle Kawamura <comrademisc@gmail.com>
2013-05-12 22:43:12 -0700

    Add validator for daily lineup -> restaurant link table.

--- app/models/scheduling.rb
+++ app/models/scheduling.rb
@@ -2,5 +2,7 @@ class Scheduling < ActiveRecord::Base
   belongs_to :daily_lineup
   belongs_to :restaurant
 
-  # attr_accessible :title, :body
+  #attr_accessible :shift, :position
+  validates_uniqueness_of :daily_lineup_id, :scope => :restaurant_id,
+                          :message => "can only have one restaurant assigned"
 end
