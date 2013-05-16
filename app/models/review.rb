26e8be27187755f50e7840a9aee9511fe75354d1
Kyle Kawamura <comrademisc@gmail.com>
2013-05-16 00:42:39 -0700

    Touch reviewable to trigger frag cache reloads.  Start validating uniqueness again.

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,6 @@
 class Review < ActiveRecord::Base
-  belongs_to :reviewable, :polymorphic => true # Polymorphic, thus not inversable
+  # Polymorphic, thus not inversable
+  belongs_to :reviewable, :polymorphic => true, :touch => true
   belongs_to :user, :inverse_of => :reviews
 
   attr_accessible :title, :content
@@ -8,8 +9,6 @@ class Review < ActiveRecord::Base
   #scope :reviews, scoped
 
   validates_presence_of :title, :content, :reviewable_id, :reviewable_type, :user_id
-=begin
   validates_uniqueness_of :user_id, :scope => [:reviewable_id, :reviewable_type],
                           :message => "user already has a review for this item"
-=end
 end
