27fd8de3254c320ae60392aca4df5483f85c6eb1
Kyle Kawamura <comrademisc@gmail.com>
2013-05-19 09:34:52 -0700

    Add User#update capability.  Currently nickname and/or password can be changed.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,12 +15,36 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
+    @user = current_user
   end
 
   def update
     @user = User.find(params[:id])
-    @user.update_aatributes(params[:user])
+    if current_user != @user
+    end
+
+    user_info = params[:user]
+    ignore_password_info = user_info[:old_password].empty? and  user_info[:password].empty? and
+                           user_info[:password_confirmation].empty?
+    if ignore_password_info
+      params[:user].delete :password
+      params[:user].delete :password_confirmation
+    else
+      passwords_match = @user.check_password(params[:user][:old_password])
+      if not passwords_match
+        @user = current_user
+        redirect_to edit_user_path(@user), :notice => "Old password does not match"
+        return
+      end
+    end
+
+    params[:user].delete :old_password
+
+    if @user.update_attributes(params[:user])
+      redirect_to root_url, notice: 'User was successfully updated.'
+    else
+      render action: "edit"
+    end
   end
 
   def activate
--- app/models/user.rb
+++ app/models/user.rb
@@ -10,13 +10,21 @@ class User < ActiveRecord::Base
 
   validates_length_of :password, :minimum => 3, :message => "password must be at least 3 characters long", :if => :password
   validates_confirmation_of :password, :message => "should match confirmation", :if => :password
-  validates_presence_of :password, :on => :create
   validates_presence_of :email
-  validates_uniqueness_of :email
+  validates_uniqueness_of :email, :case_sensitive => false
   validates_presence_of :nickname
-  validates_uniqueness_of :nickname
-  
+  validates_uniqueness_of :nickname, :case_sensitive => false
+  validates_presence_of :password, :on => :create
+
   def resend
     send_activation_needed_email!
+  end
+
+  def check_password(password)
+    self.class.send :credentials_match?, send(sorcery_config.crypted_password_attribute_name), password, salt
+  end
+
+  def set_new_hashed_password(new_password)
+    self.send :"#{sorcery_config.password_attribute_name}=", new_password
   end
 end
