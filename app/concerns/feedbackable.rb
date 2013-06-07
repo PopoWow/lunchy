fd05e408eabccc00adf6093e9ab0ea11861ef9e8
Kyle Kawamura <comrademisc@gmail.com>
2013-06-06 17:34:00 -0700

    Update feedbacks query to only return items Reviews & Reviews w/ ratings but no bare ratings anymore.

--- app/concerns/feedbackable.rb
+++ app/concerns/feedbackable.rb
@@ -11,14 +11,31 @@ module Feedbackable
   end
 
   def feedbacks
-    select_term = %Q[reviews.id AS review_id, reviews.user_id AS review_user_id, reviews.reviewable_id AS reviewable_id,
-                     ratings.id AS rating_id, ratings.user_id AS rating_user_id, ratings.ratable_id AS ratable_id]
-    select_term = "*"
-    Review.select(select_term).
-           joins(%Q[FULL OUTER JOIN ratings ON reviews.user_id = ratings.user_id]).
-                    #INNER JOIN users ON (users.id = reviews.user_id OR users.id = ratings.user_id)]).
-           where(%Q[(reviews.reviewable_id = :id AND reviews.reviewable_type = :type) OR
-                    (ratings.ratable_id    = :id AND ratings.ratable_type    = :type)],
-                 {:id => id, :type => self.class.to_s})
+    select_term ||= %Q[reviews.id AS review_id, reviews.user_id AS review_user_id, reviews.reviewable_id,
+                         reviews.title, reviews.content, reviews.updated_at AS review_udpated_at,
+                       users.id AS user_id, users.nickname,
+                       ratings.id AS rating_id, ratings.user_id AS rating_user_id, ratings.ratable_id AS ratable_id,
+                         ratings.value, ratings.updated_at AS rating_updated_at]
+    join_term = %Q[INNER JOIN reviews ON users.id = reviews.user_id AND
+                                         reviews.reviewable_id = #{id} AND
+                                         reviews.reviewable_type = '#{self.class.to_s}'
+                   LEFT OUTER JOIN ratings ON users.id = ratings.user_id AND
+                                              ratings.ratable_id = #{id} AND
+                                              ratings.ratable_type = '#{self.class.to_s}']
+    User.select(select_term).
+         joins(join_term)
+=begin
+         .
+         where(%Q[(reviews.reviewable_id = :id AND reviews.reviewable_type = :type) OR
+                  (ratings.ratable_id    = :id AND ratings.ratable_type    = :type)],
+               {:id => id, :type => self.class.to_s})
+=end
   end
 end
+=begin
+Review.select(select_term).
+       joins(%Q[FULL OUTER JOIN ratings ON reviews.user_id = ratings.user_id]).
+       where(%Q[(reviews.reviewable_id = :id AND reviews.reviewable_type = :type) OR
+                (ratings.ratable_id    = :id AND ratings.ratable_type    = :type)],
+             {:id => id, :type => type.class.to_s})
+=end
